bea4879ae7af9fc49c8d3365658ca9eb954cffc0
Carmine Massei <carminemassei@macbook-pro-di-carmine.local>
2017-06-25 18:06:04 +0200

    Initial Commit

---
+++ WatchLottery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchLottery
+//
+//  Created by Carmine Massei on 25/06/17.
+//  Copyright © 2017 iOSFoundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
