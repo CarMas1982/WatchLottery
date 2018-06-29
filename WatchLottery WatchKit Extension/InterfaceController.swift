adf53bb204cb71ad0b5c759c6c75851bfedffb70
Carmine Massei <carminemassei@macbook-pro-di-carmine.local>
2018-06-29 09:20:19 +0200

    Watch Lottery

--- WatchLottery WatchKit Extension/InterfaceController.swift
+++ WatchLottery WatchKit Extension/InterfaceController.swift
@@ -10,22 +10,83 @@ import WatchKit
 import Foundation
 
 
+
+
 class InterfaceController: WKInterfaceController {
+    
+    var nums = [1,2,3,4,5,6,7,8,9,10,
+                11,12,13,14,15,16,17,18,19,20,
+                21,22,23,24,25,26,26,28,29,30]
+    var num = 0
+    
+    
+    
+    let lottery = WatchLottery()
+    
+    @IBOutlet var result: WKInterfaceLabel!
+    
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
+        //showFortune()
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
+        
         super.willActivate()
+        //showFortune()
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    @IBAction func randomNumber() {
+//        while nums.count > 0 {
+//            
+//            // random key from array
+//            let arrayKey = Int(arc4random_uniform(UInt32(nums.count)))
+//            
+//            // your random number
+//            let randNum = nums[arrayKey]
+//            
+//            
+//            // make sure the number isnt repeated
+//            nums.remove(at: arrayKey)
+//            //print(randNum)
+//            num = randNum
+//        }
+//        result.setText("\(num)")
+        //showFortune()
+        let numbers = lottery.generateNumbers(repetitions: 5, maxValue: 30)
+        result.setText("\(numbers)")
+//        for i in numbers{
+//        result.setText("\(i)")
+//        }
+        
+    }
+    
+    
+    func showFortune(){
+        while nums.count > 0 {
+            
+            // random key from array
+            let arrayKey = Int(arc4random_uniform(UInt32(nums.count)))
+            
+            // your random number
+            let randNum = nums[arrayKey]
+            
+            
+            // make sure the number isnt repeated
+            nums.remove(at: arrayKey)
+            //print(randNum)
+            num = randNum
+        }
+        print(num)
+        result.setText("\(num)")
     }
 
 }
--- WatchLottery/ViewController.swift
+++ WatchLottery/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var result: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func random(_ sender: UIButton) {
+        let cr = GKShuffledDistribution(lowestValue: 1, highestValue: 30)
+        result.text = "\(cr.nextInt())"
+    }
 
 }
 
